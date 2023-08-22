cbab65453069382a8408ab399b7c3a876c45b78f
Anna <trembach.ann@gmail.com>
2023-08-22 15:49:34 +0300

    added the backend part of the registration panel

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import multer from "multer";
-import { error } from "console";
+//import { error } from "console";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
 })
 const upload = multer({storage});
  
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture", register));
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL,{
